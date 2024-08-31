bd28eb20c0c64fcc3bc5bd357e93a970bd780e14
ByteAtATime <byteatatime@proton.me>
2024-08-31 12:18:23 -0700

    feat: inline copy icons to obey currentColor

--- src/copy-address/copy-address.component.ts
+++ src/copy-address/copy-address.component.ts
@@ -1,9 +1,8 @@
 import { customElement, property, state } from "lit/decorators.js";
 import { html, LitElement } from "lit";
-import copyAddress from "./copy.svg";
-import copyAddressSuccess from "./copy-success.svg";
 import copyAddressStyle from "~/copy-address/copy-address.style.ts";
 import { Address } from "viem";
+import { copyIcon, copySuccessIcon } from "~/copy-address/copy-icons.ts";
 
 @customElement("copy-address")
 export class CopyAddressComponent extends LitElement {
@@ -16,12 +15,9 @@ export class CopyAddressComponent extends LitElement {
   addressCopied = false;
 
   render() {
-    const buttonImage = this.addressCopied ? copyAddressSuccess : copyAddress;
-    const buttonAlt = this.addressCopied ? "Address copied" : "Copy address";
+    const buttonImage = this.addressCopied ? copySuccessIcon : copyIcon;
 
-    return html`<button @click=${this.copyAddress}>
-      <img alt=${buttonAlt} src=${buttonImage} />
-    </button>`;
+    return html`<button @click=${this.copyAddress}>${buttonImage}</button>`;
   }
 
   private async copyAddress() {
